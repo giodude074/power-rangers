2d754c1a2e26ded66a70fb5cc6e21a2cc595096b
Max Ziwei Xiong <max.zw.xiong@gmail.com>
2019-12-09 17:25:38 -0800

    fixed database

--- app/src/main/java/com/example/petfindr/MainFragment.kt
+++ app/src/main/java/com/example/petfindr/MainFragment.kt
@@ -32,6 +32,7 @@ class MainFragment : Fragment() {
         // Inflate the layout for this fragment
 
         return binding.root
+
     }
 
 
--- app/src/main/java/com/example/petfindr/PostFragment.kt
+++ app/src/main/java/com/example/petfindr/PostFragment.kt
@@ -1,15 +1,24 @@
 package com.example.petfindr
 
 import android.content.Context
+import android.location.Location
 import android.net.Uri
 import android.os.Bundle
+import android.util.EventLogTags
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.databinding.DataBindingUtil
 import com.example.petfindr.databinding.FragmentPostBinding
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.fragment_post.*
+import androidx.databinding.adapters.NumberPickerBindingAdapter.setValue
+import com.google.firebase.database.DatabaseReference
+
+
+
 
 class CreateFragment : Fragment() {
 
@@ -28,21 +37,26 @@ class CreateFragment : Fragment() {
         val PostButton = binding.postButton
 
         PostButton.setOnClickListener {view: View->
-            val PetName = binding.nameBox
-            val Location = binding.locationBox
-            val Descrtion = binding.locationBox
 
+            val database = FirebaseDatabase.getInstance()
+            val petName = name_box.text.toString()
+            val location = location_box.text.toString()
+            val desc = description_box.text.toString()
 
-
-
+            val myRef = database.getReference("name")
+            val myRef1 = database.getReference("location")
+            val myRef2 = database.getReference("Descritpion")
+            myRef.setValue(petName)
+            myRef1.setValue(location)
+            myRef2.setValue(desc)
 
         }
 
         // Write a message to the database
-        val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("message")
 
-        myRef.setValue("Hello, World!")
+
+
+
 
 
 
